abb0febd82ffe466c8572c8a6200a4fdbd6f94dd
Reyad07 <latif.tajnoor@gmail.com>
2024-03-07 10:27:08 +0800

    Display Time

---
+++ Displaytime.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Displaytime {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.print("please input seconds:");
+        int seconds = input.nextInt();
+
+        int minutes = seconds / 60;
+        int remainingSeconds = seconds % 60;
+
+        System.out.println(minutes + ":" + remainingSeconds);
+    }
+    
+}
