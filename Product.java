2338a2eb1a93b750527548fcf4c2aabdcfdbb46d
Reyad07 <latif.tajnoor@gmail.com>
2024-03-15 19:20:11 +0800

    Assignment 1

---
+++ Product.java
@@ -0,0 +1,10 @@
+public class Product{
+    public static void main (String[] args){
+        System.out.println("ID: 101");
+        System.out.println("Title: Iphone 15");
+        System.out.println("Price: 1895 Euros");
+        System.out.println("Description: Perfect product wiith best image quality");
+        System.out.println("Category: Phone");
+    }
+
+}
